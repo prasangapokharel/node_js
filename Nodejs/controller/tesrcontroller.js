c07f5a88daea6fb3acf829cd2ebefbbb23512603
Prasanga Pokharel <138413854+prasangapokharel@users.noreply.github.com>
2024-01-14 09:17:36 +0545

    Add files via upload

---
+++ Nodejs/controller/tesrcontroller.js
@@ -0,0 +1,5 @@
+exports.testfuntion = async,(req, res) =>{
+    res.status(4401),json({
+        message: "this is from test function"
+    })
+}
---
+++ Nodejs/server.js
@@ -0,0 +1,32 @@
+
+// server.js
+
+const express = require('express');
+
+const app = express();
+// const prasangaLogin = require('./prasanga.js');
+// const uiPage = require('./ui.js');
+
+
+const db = require('./model/index');
+db.sequelize.sync({ force: false });
+
+app.use(express.json());
+app.use(express.urlencoded({ extende: true }));
+const createRouter = require("./routes/blogs.js");
+
+app.use("/api", createRouter);
+
+
+const port = 3000;
+
+// Use the login route from prasanga.js
+// app.use('/', prasangaLogin);
+
+// // Use the UI route from ui.js
+// app.use('/', uiPage);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
